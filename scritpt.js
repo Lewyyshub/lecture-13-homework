fb67a07ac8a5a7baad2393e5766b678c1329598e
Lewyyshub <salamashvili21@gmail.com>
2024-10-18 02:47:49 +0400

    lecture 13 homework

---
+++ scritpt.js
@@ -0,0 +1,12 @@
+const name = "Levan ";
+const lastname = "Salamashvili ";
+let age = 22;
+const email = " salamashvili21@gmail.com ";
+const number = 593122703;
+const adult = true;
+
+let ertad = name + lastname + age + email + number + adult;
+
+console.log(ertad);
+console.log("%ctrue", "color:yellow");
+console.log("%c593122703", "color:red");
